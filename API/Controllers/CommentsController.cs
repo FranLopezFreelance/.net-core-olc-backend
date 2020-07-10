c43e2ebff079269b961b72fe2ce1e9ae1681366c
frLopez <frlopez@WKS928L>
2020-07-10 13:15:00 -0300

    Add CreationDate, and new migration files to modify tables

---
+++ API/Controllers/CommentsController.cs
@@ -0,0 +1,26 @@
+﻿using Application.Comments;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class CommentsController: ApiBaseController
+    {
+        //https://localhost:44364/api/comments
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Create(Create.Execute data)
+        {
+            return await Mediator.Send(data);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Unit>> Delete(Guid id)
+        {
+            return await Mediator.Send(new Delete.Execute { CommentId = id });
+        }
+    }
+}
